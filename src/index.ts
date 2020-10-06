24b58d27264e210703afa9a2284bc526f61e98a7
Alexander Granhof <alexander.g737@gmail.com>
2020-10-06 23:38:44 +0200

    eslint format

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 // Imports
-import * as Discord from 'discord.js'
+import * as Discord from "discord.js";
 
-import auth from './auth/auth.json'
-import settings from './settings/settings.json'
+// import auth from './auth/auth.json'
+// import settings from './settings/settings.json'
 
-import MessageHandler from './module/MessageHandler'
+import MessageHandler from "./modules/handlers/message";
 
 // Creates client interface
-const client = new Discord.Client()
-const mh = new MessageHandler()
+const client = new Discord.Client();
+const mh = new MessageHandler();
 
 // Logs ready message
-client.on('ready', () => {
-    console.log('Fika Bot is now serving dammsugare!')
-})
+client.on("ready", () => {
+    console.log("Fika Bot is now serving dammsugare!");
+});
 
 // Handle commands
-client.on('message', (message) => {
-    mh.handle(message)
-})
+client.on("message", (message) => {
+    mh.handle(message);
+});
 
 // Login bot with token
-client.login(auth.token)
+// client.login(auth.token);
