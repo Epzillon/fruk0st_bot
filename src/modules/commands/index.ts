52d757f14f5f175e2fe86a30d6c4d49e26024ea4
Alexander Granhof <alexander.g737@gmail.com>
2020-10-06 23:37:35 +0200

    renamed commands into index, easier import

---
+++ src/modules/commands/index.ts
@@ -0,0 +1,8 @@
+import { Command } from "../../models/commands";
+
+import react from "./react";
+import help from "./help";
+
+const commands: Command[] = [help, react];
+
+export default commands;
