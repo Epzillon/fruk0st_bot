f628526f6364223589db64b827a37e92a79744ab
Alexander Granhof <alexander.g737@gmail.com>
2020-10-06 23:23:01 +0200

    moved bot into src and named it as index

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// Imports
+import * as Discord from 'discord.js'
+
+import auth from './auth/auth.json'
+import settings from './settings/settings.json'
+
+import MessageHandler from './module/MessageHandler'
+
+// Creates client interface
+const client = new Discord.Client()
+const mh = new MessageHandler()
+
+// Logs ready message
+client.on('ready', () => {
+    console.log('Fika Bot is now serving dammsugare!')
+})
+
+// Handle commands
+client.on('message', (message) => {
+    mh.handle(message)
+})
+
+// Login bot with token
+client.login(auth.token)
