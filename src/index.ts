4d9e3e6701d91b7823e460494067bb8d6330bc2c
Jakob Eriksson <jakoberiksson99@hotmail.com>
2020-10-16 13:28:58 +0200

    Documented and restructured constants/env.ts. Fixed invalid key:value combination from #3

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
 // Imports
 import * as Discord from "discord.js";
-import dotenv from "dotenv";
 import chalk from "chalk";
 import constants from "./modules/constants";
-
-const { DISCORD_TOKEN, DEV_ID, NODE_ENV } = constants;
-
-// Loads env variables from a .env file
-dotenv.config();
-
-// If we are missing the discord token, throw error
-if (DISCORD_TOKEN) {
-    console.error(chalk.red("Missing .env variable 'DISCORD_TOKEN'. Create .env file or add it to the environment"));
-    process.exit(1);
-}
 
 import MessageHandler from "./modules/handlers/message";
 
@@ -29,8 +17,8 @@ client.on("ready", () => {
 // Handle commands
 client.on("message", (message) => {
     // Only listen to developer message in development mode.
-    if (NODE_ENV === "dev") {
-        if (message.author.id === DEV_ID) {
+    if (constants.NODE_ENV === "dev") {
+        if (message.author.id === constants.DEV_ID) {
             mh.handle(message);
         }
         // Never listen to other bots
@@ -40,4 +28,4 @@ client.on("message", (message) => {
 });
 
 // Login bot with token
-client.login(DISCORD_TOKEN);
+client.login(constants.DISCORD_TOKEN);
