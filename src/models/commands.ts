6c6ae4547de55459f20b14e7b10c0d41d9405ef2
Jakob Eriksson <jakoberiksson99@hotmail.com>
2022-01-02 18:16:18 +0100

    Remaking commands.ts model to an interface

--- src/models/commands.ts
+++ src/models/commands.ts
@@ -1,11 +1,13 @@
 import * as Discord from "discord.js";
 
 /**
- * A Command model. Used for defining and handling commands.
+ * A Command interface. Used for defining and handling commands.
  */
-export type Command = {
+interface Command {
     name: string;
     description: string;
     usage: string[];
     execute: (message: Discord.Message) => void;
-};
+}
+
+export default Command;
