657a42534edf310fa59e946843116b39c2fbf6f0
Jakob Eriksson <jakoberiksson99@hotmail.com>
2022-01-02 19:15:38 +0100

    Rebase interface and classes (#6)
    * Remaking commands.ts model to an interface
    
    * Updated command files to incorporate interfaces and classes
    
    * Added new log prints to clarify when running development mode.

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,22 @@ const mh = new MessageHandler();
 // Logs ready message
 client.on("ready", () => {
     console.log(chalk.green("Fruk0st Bot is now serving lunch!"));
+
+    if (constants.NODE_ENV === "dev") {
+        console.log(chalk.white("INFO: Developer mode is enabled."));
+        console.log(chalk.yellow("WARNING: DO NOT USE DEVELOPER MODE IN PRODUCTION."));
+    }
 });
 
 // Handle commands
 client.on("message", (message) => {
-    // Only listen to developer message in development mode.
     if (constants.NODE_ENV === "dev") {
         if (message.author.id === constants.DEV_ID) {
             mh.handle(message);
         }
-        // Never listen to other bots
     } else if (!message.author.bot) {
         mh.handle(message);
     }
 });
 
-// Login bot with token
 client.login(constants.DISCORD_TOKEN);
