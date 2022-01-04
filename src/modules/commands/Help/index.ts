fe49f9ae2a48151fdf2215b9e488dcb0d6a4b618
Jakob Eriksson <jakoberiksson99@hotmail.com>
2022-01-04 20:51:42 +0100

    Updated Help command to extend AbstractCommand

--- src/modules/commands/Help/index.ts
+++ src/modules/commands/Help/index.ts
@@ -1,8 +1,7 @@
 import * as Discord from "discord.js";
+import AbstractCommand from "../../../models/AbstractCommand";
 
-import CommandInterface from "../../../models/CommandInterface";
-
-class Help implements CommandInterface {
+class Help extends AbstractCommand {
     public name = "help";
     public description = "Sends this message in DM's";
     public usage = [""];
@@ -13,9 +12,10 @@ class Help implements CommandInterface {
      * @param {Discord.Message} message The Discord message which called upon the command.
      */
     public execute(message: Discord.Message): void {
+        super.execute(message);
+
         const author = message.author;
 
-        // Send the help text to user who called "help" in DM.
         author.send(
             "Hello, stranger! You can find the descriptions and usage instructions for commands here:\nhttps://github.com/EpiX0R/fruk0st_bot/blob/master/COMMANDS.md",
         );
