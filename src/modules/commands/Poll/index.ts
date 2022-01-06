ed26ec63bb9df8791a372b01fbe4b5ae6a86d084
Jakob Eriksson <jakoberiksson99@hotmail.com>
2022-01-06 19:14:36 +0100

    Updated Poll command to extend AbstractCommand

--- src/modules/commands/Poll/index.ts
+++ src/modules/commands/Poll/index.ts
@@ -1,28 +1,20 @@
 import * as Discord from "discord.js";
-import fs from "fs";
-import Path from "path";
+import AbstractCommand from "../../../models/AbstractCommand";
 
-import CommandInterface  from "../../../models/CommandInterface";
-import settings from "../../../settings.json";
-
-import constants from "../../constants";
-
-class Poll implements CommandInterface {
+class Poll extends AbstractCommand {
     public name = "poll";
     public description = "Allows you to manage polls.";
     public usage = ["create <?date> <time> <answers>", "delete <id>", "<id>"];
 
     /**
-     * Handles the execution of the "react" command and its sub-commands.
+     * Handles the execution of the "poll" command and its sub-commands.
      *
      * @param {Discord.Message} message The Discord message which called the command.
      */
     public execute(message: Discord.Message): void {
-        const commandStr = message.content.split(constants.COMMAND_PREFIX)[1];
-        const parameters = commandStr.split(" ");
+        const commandParameters = super.getCommandParameters();
 
-        // Switch based on sub-command
-        switch (parameters[1]) {
+        switch (commandParameters[1]) {
             default:
                 break;
         }
